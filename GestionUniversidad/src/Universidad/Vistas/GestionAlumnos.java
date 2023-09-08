840a05ef37f748bed28162de8afd86855b72dfb6
ianrodriguezy <ianrodriguez.y@gmail.com>
2023-09-08 12:37:44 -0300

    d2ult

--- GestionUniversidad/src/Universidad/Vistas/GestionAlumnos.java
+++ GestionUniversidad/src/Universidad/Vistas/GestionAlumnos.java
@@ -4,10 +4,17 @@
  */
 package Universidad.Vistas;
 
-/**
- *
- * @author Ian
- */
+import Universidad.AccesoaDatos.Conectar;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.ZoneId;
+        
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+
+
 public class GestionAlumnos extends javax.swing.JInternalFrame {
 
     /**
@@ -33,16 +40,16 @@ public class GestionAlumnos extends javax.swing.JInternalFrame {
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jRadioButton1 = new javax.swing.JRadioButton();
+        jtDoc = new javax.swing.JTextField();
+        jtApellido = new javax.swing.JTextField();
+        jtNombre = new javax.swing.JTextField();
+        jrbEstado = new javax.swing.JRadioButton();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jbNuevoAlumno = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jDateChooser1 = new com.toedter.calendar.JDateChooser();
+        jbSalir = new javax.swing.JButton();
+        jdFechaNac = new com.toedter.calendar.JDateChooser();
 
         jLabel1.setText("Alumno");
 
@@ -56,9 +63,18 @@ public class GestionAlumnos extends javax.swing.JInternalFrame {
 
         jLabel6.setText("Fecha de Nacimiento");
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        jtDoc.setToolTipText("");
+        jtDoc.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                jtDocActionPerformed(evt);
+            }
+        });
+        jtDoc.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jtDocKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jtDocKeyTyped(evt);
             }
         });
 
@@ -69,10 +85,10 @@ public class GestionAlumnos extends javax.swing.JInternalFrame {
             }
         });
 
-        jButton2.setText("Nuevo");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jbNuevoAlumno.setText("Nuevo");
+        jbNuevoAlumno.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jbNuevoAlumnoActionPerformed(evt);
             }
         });
 
@@ -80,7 +96,12 @@ public class GestionAlumnos extends javax.swing.JInternalFrame {
 
         jButton4.setText("Guardar");
 
-        jButton5.setText("Salir");
+        jbSalir.setText("Salir");
+        jbSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbSalirActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPgestionLayout = new javax.swing.GroupLayout(jPgestion);
         jPgestion.setLayout(jPgestionLayout);
@@ -98,15 +119,15 @@ public class GestionAlumnos extends javax.swing.JInternalFrame {
                 .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPgestionLayout.createSequentialGroup()
                         .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
-                            .addComponent(jTextField3)
-                            .addComponent(jTextField2))
+                            .addComponent(jtDoc, javax.swing.GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
+                            .addComponent(jtNombre)
+                            .addComponent(jtApellido))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton1))
                     .addGroup(jPgestionLayout.createSequentialGroup()
                         .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jDateChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jRadioButton1))
+                            .addComponent(jdFechaNac, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jrbEstado))
                         .addGap(0, 0, Short.MAX_VALUE))))
             .addGroup(jPgestionLayout.createSequentialGroup()
                 .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -115,13 +136,13 @@ public class GestionAlumnos extends javax.swing.JInternalFrame {
                         .addComponent(jLabel1))
                     .addGroup(jPgestionLayout.createSequentialGroup()
                         .addGap(99, 99, 99)
-                        .addComponent(jButton2)
+                        .addComponent(jbNuevoAlumno)
                         .addGap(29, 29, 29)
                         .addComponent(jButton3)
                         .addGap(31, 31, 31)
                         .addComponent(jButton4)
                         .addGap(31, 31, 31)
-                        .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jbSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(63, Short.MAX_VALUE))
         );
         jPgestionLayout.setVerticalGroup(
@@ -131,34 +152,34 @@ public class GestionAlumnos extends javax.swing.JInternalFrame {
                 .addComponent(jLabel1)
                 .addGap(55, 55, 55)
                 .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtDoc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1)
                     .addComponent(jLabel2))
                 .addGap(28, 28, 28)
                 .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jtApellido, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(28, 28, 28)
                 .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPgestionLayout.createSequentialGroup()
                         .addGap(26, 26, 26)
                         .addComponent(jLabel5))
                     .addGroup(jPgestionLayout.createSequentialGroup()
                         .addGap(18, 18, 18)
-                        .addComponent(jRadioButton1)))
+                        .addComponent(jrbEstado)))
                 .addGap(24, 24, 24)
                 .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel6)
-                    .addComponent(jDateChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jdFechaNac, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(42, 42, 42)
                 .addGroup(jPgestionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
+                    .addComponent(jbNuevoAlumno)
                     .addComponent(jButton3)
                     .addComponent(jButton4)
-                    .addComponent(jButton5))
+                    .addComponent(jbSalir))
                 .addContainerGap(48, Short.MAX_VALUE))
         );
 
@@ -178,26 +199,71 @@ public class GestionAlumnos extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void jtDocActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtDocActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_jtDocActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jbNuevoAlumnoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbNuevoAlumnoActionPerformed
+        Connection con=null;
+        PreparedStatement ps=null;
+        boolean ver=false;
+        int aux;
+        do{
+        do {
+            if(jtDoc.getText().isEmpty()||jtApellido.getText().isEmpty()||jtNombre.getText().isEmpty()||jdFechaNac.equals("")){
+                JOptionPane.showMessageDialog(null, "Todos o alguno de los campos se encuentran vacios, por favor rellene todos.");
+                break;
+            }else{
+                ver=true;}
+        }while (!ver);
+        }while(!ver);
+        if(jrbEstado.isSelected()){
+            aux=1;
+        }else{aux=0;}
+        LocalDate fecha= jdFechaNac.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        java.sql.Date jdate = java.sql.Date.valueOf(fecha);
+        
+        
+        String sql= "INSERT INTO `alumno`(`dni`, `apellido`, `nombre`, `fechaNacimiento`, `estado`) VALUES ('"+jtDoc.getText()+"','"+jtApellido.getText()+"','"+jtNombre.getText()+"','"+jdate+"','"+aux+"')";
+        con=Conectar.getConectar();
+        
+        try{
+            ps= con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.executeUpdate();
+            ResultSet rs= ps.getGeneratedKeys();
+//            if(rs.next()){
+//                a.setIdAlumno(rs.getInt(1));
+//                System.out.println("Alta exitosa");
+//            }
+        }catch(SQLException x){
+            System.out.println("Error "+ x.getMessage());
+            
+        }
+    }//GEN-LAST:event_jbNuevoAlumnoActionPerformed
+
+    private void jbSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbSalirActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jbSalirActionPerformed
+
+    private void jtDocKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtDocKeyReleased
+        
+    }//GEN-LAST:event_jtDocKeyReleased
+
+    private void jtDocKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtDocKeyTyped
+       if(jtDoc.getText().length()>=8){
+           evt.consume();
+       }
+    }//GEN-LAST:event_jtDocKeyTyped
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private com.toedter.calendar.JDateChooser jDateChooser1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -205,9 +271,12 @@ public class GestionAlumnos extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPgestion;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JButton jbNuevoAlumno;
+    private javax.swing.JButton jbSalir;
+    private com.toedter.calendar.JDateChooser jdFechaNac;
+    private javax.swing.JRadioButton jrbEstado;
+    private javax.swing.JTextField jtApellido;
+    private javax.swing.JTextField jtDoc;
+    private javax.swing.JTextField jtNombre;
     // End of variables declaration//GEN-END:variables
 }
